7fe848bf9082413a26bc2fde5dc4925cf50fb4c5
aldinbatocabe <106215295+aldinbatocabe@users.noreply.github.com>
2022-06-12 10:56:52 -0500

    Update week2.js

--- week2.js
+++ week2.js
@@ -1,18 +1,26 @@
-let howManyCats = prompt("How many cats do you have?");
+// let howManyCats = prompt("How many cats do you have?");
 
-console.log(howManyCats);
+// console.log(howManyCats);
 
-if (howManyCats > 50) {
-    alert("You have too many cats, get help!");
-} else if (howManyCats >= 1) {
-    alert("We're best friends!");
+// if (howManyCats > 50) {
+//     alert("You have too many cats, get help!");
+// } else if (howManyCats >= 1) {
+//     alert("We're best friends!");
+// } else {
+//     alert("Go away!");
+// }
+
+// while (howManyCats < 5) {
+//     howManyCats++;
+//     console.log("Here's another cat!");
+// }
+// alert("Now, you have the right amount of cats: " + howManyCats);
+
+let num = prompt("Guess the number");
+
+if (num == 11) {
+    alert("You have the correct number!");
 } else {
-    alert("Go away!");
+    alert("Try again");
 }
-
-while (howManyCats < 5) {
-    howManyCats++;
-    console.log("Here's another cat!");
-}
-alert("Now, you have the right amount of cats: " + howManyCats);
 
